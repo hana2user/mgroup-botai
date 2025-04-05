3e6452750a908e25d3a5281d17fa7f60947b2469
Georgy Levin <g.phyzy@gmail.com>
2025-04-05 11:34:08 +0300

    fixed KEY API

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,13 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDMt1xmjz7bsrdVsLVcQzrfciwrYQkdl1Y",
+    apiKey: process.env.FB_API_KEY,
     authDomain: "mgroup-3cf26.firebaseapp.com",
     projectId: "mgroup-3cf26",
     storageBucket: "mgroup-3cf26.firebasestorage.app",
     messagingSenderId: "253850307133",
-    appId: "1:253850307133:web:7da7cd80f79535a13cf854"
+    appId: "1:253850307133:web:86df02e9e69fb3ad3cf854"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
