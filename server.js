65846ace7d3c9d2523bb91c71c38bfab1091f153
Georgy Levin <g.phyzy@gmail.com>
2025-04-06 16:35:10 +0300

    corsOptions for prod

--- server.js
+++ server.js
@@ -2,26 +2,31 @@ import express from 'express';
 import cors from 'cors';
 import { generateContent } from './src/services/generateContent.js';
 
+const corsOptions = {
+    origin: ['https://mgroup.onrender.com', 'http://localhost:3000'],
+};
+
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors(corsOptions));
 app.use(express.json());
 const port = 3000;
 
 app.get('/hello', (req, res) => {
-    res.send('Hello World from Express!')});
+    res.send('Hello World from Express!')
+});
 
 app.post('/content', async (req, res) => {
     try {
         const { message } = req.body;
         const content = await generateContent(message);
-        if ( content == null || content === '') {
+        if (content == null || content === '') {
             content = 'No content was received.';
         }
         res.status(200).json({ content });
     } catch (error) {
         console.error("Error generating content:", error);
         res.status(500).json({ error: "An error occurred while generating content." });
-    } 
+    }
 });
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
